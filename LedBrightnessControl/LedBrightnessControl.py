05c3f28aebc103550e4ecd2cee30f05dde99d4e6
Gül Hilal Atalay <67538641+gulhilal@users.noreply.github.com>
2023-07-04 14:25:42 +0300

    Update LedBrightnessControl.py

--- LedBrightnessControl/LedBrightnessControl.py
+++ LedBrightnessControl/LedBrightnessControl.py
@@ -5,73 +5,72 @@ import HandTrackingModule as htm
 import math
 import serial
 
-arduino = serial.Serial('COM4', 9600)  # com4'ü açar 9600 baudrate ile bağlanır
+arduino = serial.Serial('COM4', 9600)  # opens com4 connects with baudrate of 9600
 
 ########################
-wCam, hCam = 640, 480    # kamera penceresinin genişliği ve uzunluğu belirlenir
+wCam, hCam = 640, 480    # the width and length of the camera window are determined
 ########################
 
-cap = cv2.VideoCapture(0)  # webcam'in açılması
+cap = cv2.VideoCapture(0)  # opening webcam
 cap.set(3, wCam)
 cap.set(4, hCam)
-pTime = 0  # FPS hesaplanırken kullnaıldı
+pTime = 0  # used when calculating FPS
 
-detector = htm.handDetector(detectionCon=0.7)  # HandTrackingModule kütüphanesindeki handDetector sınıfından bir obje oluşturuldu
+detector = htm.handDetector(detectionCon=0.7)  # Created an object from the handDetector class in the HandTrackingModule library
 
-brightnessBar = 150  # parlaklık seviyesini gösteren bar başlangıçta en düşük seviyede tanımlandı
-brightnessPer = 0      # parlaklık seviyesi başlangıçta en düşük seviyede tanımlandı
-
+brightnessBar = 150  # bar showing the brightness level was initially defined at the lowest level
+brightnessPer = 0      # the brightness level was initially defined at the lowest level
 while True:
     success, img = cap.read()
-    img = detector.findHands(img)  # findHands metodu ile kamerada el tespit edildi ve img'a atandı
-    PosList = detector.findPosition(img, draw=False)   # PosList'e findPosition'dan gelen liste atandı.
-    if len(PosList) != 0:                              # Bu listede elin 20 noktasının pozisyonları bulunuyor
-        x1, y1 = PosList[4][1], PosList[4][2]   # 4 baş parmağı 8 de işaret parmağını temsil ediyor bu yüzden bu noktaların x ve y
-        x2, y2 = PosList[8][1], PosList[8][2]   # değerleri değişkenlere atanıyor
-        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2  # iki noktanın ortası bulunuyor
+    img = detector.findHands(img)  # Hand detected on camera with findHands method and assigned to img
+    PosList = detector.findPosition(img, draw=False)   # List from findPosition is assigned to PosList.
+    if len(PosList) != 0:                              # This list contains the positions of the 20 points of the hand.
+        x1, y1 = PosList[4][1], PosList[4][2]   # 4 thumbs represent 8 index fingers so x and y values of these points are assigned to variables
+        x2, y2 = PosList[8][1], PosList[8][2]   
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2  # find the middle of two points and assign cx and cy
 
-        cv2.circle(img, (x1, y1), 10, (255, 0, 255), cv2.FILLED)  # Baş parmak işaret parmağı ve merkeze daire çiziliyor
+        cv2.circle(img, (x1, y1), 10, (255, 0, 255), cv2.FILLED)  # Thumb, forefinger and center circled
         cv2.circle(img, (x2, y2), 10, (255, 0, 255), cv2.FILLED)
         cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)  # Baş parmak ile işaret parmağı arasına çizgi çiziliyor
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)  # Drawing a line between thumb and forefinger
 
 
-        length = int(math.hypot(x2 - x1, y2 - y1))  # baş parmak ile işaret parmağı arasındaki uzunluk bulunuyor
+        length = int(math.hypot(x2 - x1, y2 - y1))  # the length between thumb and index finger is found and assigned to length
         # print (length)
         # print(length.bit_length())
 
-        if length < 30:  # parmaklar tam kapanınca merkezdeki dairenin rengi yeşil oluyor
+        if length < 30:  # When the fingers are fully closed, the color of the circle in the center turns green.
             cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
 
 
-        length = int(np.interp(length, [30, 280], [0, 255]))  # 30-280 arasında değişen uzunluk değerleri 0-255'e dönüştürülüyor.
-                                                              # Bu değerler seri porta gönderileceği için 0-255 arasında olmalı.
+        length = int(np.interp(length, [30, 280], [0, 255]))  # Length values ranging from 30-280 are converted to 0-255.
+                                                              # Since these values will be sent to the serial port, they should be between 0-255.
 
-        brightnessBar = np.interp(length, [0, 255], [400, 150]) # parlaklık seviyesinde 0->400'e 255->150 ye dönüştürülüyor.
-        brightnessPer = np.interp(length, [0, 255], [0, 100])  # yüzde parlaklık seviyesinde 0->0'a 255->100'e dönüştürülüyor
+        brightnessBar = np.interp(length, [0, 255], [400, 150]) # It is converted to 0->400 at brightness level to 255->150.
+        brightnessPer = np.interp(length, [0, 255], [0, 100])  # converting from 0->0 to 255->100 at percent brightness level
 
-        cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 255), 3)  # (50, 150), (85, 400) konumunda bir dikdörtgen oluşturuluyor
-        cv2.rectangle(img, (50, int(brightnessBar)), (85, 400), (255, 0, 255), cv2.FILLED) # oluşturulan dikdörtgenin içi parlaklık seviyesine göre dolduruluyor.
-        cv2.putText(img, f'{int(brightnessPer)}%', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2) # parlaklık yüzdesi oluşturulan dikdörtgen barın altına yazılıyor.
-
-
-        arduino.write(bytes([length])) # iki parmak arasındaki uzunluk değeri arduinoya gönderiliyor
-
-        print(f'Seri Porta Gönderilen Deger: ', length) # seri porta gönderilen değer ekrana yazdırılıyor
+        cv2.rectangle(img, (50, 150), (85, 400), (255, 0, 255), 3)  # Creating a rectangle at (50, 150), (85, 400)
+        cv2.rectangle(img, (50, int(brightnessBar)), (85, 400), (255, 0, 255), cv2.FILLED) # The inside of the created rectangle is filled according to the brightness level.
+        cv2.putText(img, f'{int(brightnessPer)}%', (40, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2) # The brightness percentage is written under the created rectangular bar.
 
 
-        if length == 0:  # seri porta gönderilen değer 0 ise LED SONDU yazısı palaklık yüzddesinin yanında yazıyor
-            cv2.putText(img, f'(LED SONDU.)', (100, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 2)
+        arduino.write(bytes([length])) # length value between two fingers is sent to arduino
 
-        if length == 255:  # seri porta gönderilen değer 255 ise MAXIMUM PARLAKLIK yazısı palaklık yüzddesinin yanında yazıyor
-            cv2.putText(img, f'(MAXIMUM PARLAKLIK.)', (140, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 2)
+        print(f'Value Sent to Serial Port: ', length) # the value sent to the serial port is printed 
 
 
-    cTime = time.time()  # saniye cinsinden şimdiki zamanı veriyor
-    fps = 1 / (cTime - pTime) # FPS hesaplanıyor
+        if length == 0:  # If the value sent to the serial port is 0, LED OFF is written next to the brightness percentage.
+            cv2.putText(img, f'(LED OFF.)', (100, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 2)
+
+        if length == 255:  # If the value sent to the serial port is 255, MAXIMUM BRIGHTNESS is written next to the brightness percentage.
+            cv2.putText(img, f'(MAXIMUM BRIGHTNESS.)', (140, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 2)
+
+
+    cTime = time.time()  # Returns the current time in seconds
+    fps = 1 / (cTime - pTime) # Calculating FPS
     pTime = cTime
 
-    cv2.putText(img, f'FPS:{int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3) # FPS değeri ekranın sol üst köşesine yazdırılıyor.
+    cv2.putText(img, f'FPS:{int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3) # FPS value is printed in the upper left corner of the screen.
 
     cv2.imshow("Img", img)
     cv2.waitKey(1)
